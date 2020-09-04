ac307d18f1532956688727fd8dca1578b3489180
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-09-04 11:30:24 -0400

    init

---
+++ src/main/java/org/tampasa/kettles/KettlesApplication.java
@@ -0,0 +1,13 @@
+package org.tampasa.kettles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KettlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KettlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tampasa/kettles/KettlesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tampasa.kettles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KettlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
