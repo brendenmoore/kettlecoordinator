ec6bc65bab4a8a03f380b291665bbbe0ba5b872d
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-11-20 17:39:13 -0500

    change navbar to boostrap

--- kettles-client/src/app/shared/navbar/navbar.component.ts
+++ kettles-client/src/app/shared/navbar/navbar.component.ts
@@ -3,8 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
 
